18befaa4d904cebe3a2ae7d5b4a21d94656eea78
newcometlab <newcometlab@outlook.com>
2021-01-28 11:26:23 -0800

    01-get weather api data

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
+// import { WeatherContext } from './WeatherContext';
+import axios from "axios";
+
+
+const API_KEY = 'e1ec43e71a98cc14a4799aabd7b70c27';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,10 +32,42 @@ const useStyles = makeStyles((theme) => ({
 
 const SearchField = () => {
     const classes = useStyles();
+    const [name, setName] = useState('');
+    // const [city, setCity] = useContext(WeatherContext);
+    const [city, setCity] = useState('');
+    const [weatherData, setWeatherData] = useState([]);
+
+
+    const getWeather = async () => {
+        const arr = [];
+
+        try {
+            const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+            const weatherRes = await axios.get(weatherUrl);
+
+            console.log('weatherRes: ', weatherRes);
+            console.log(arr.push(weatherRes.data.weather))
+            arr.push(weatherRes.data.weather);
+            setWeatherData(arr);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const handleSearchChange = e => {
+        setCity(e.target.value);
+    }
+
+    const handleSearch = e => {
+        e.preventDefault();
+        getWeather();
+    }
+
     return (
         <div>
-            <form >
-                <Paper
+            <form onSubmit={handleSearch}>
+                {/*<Paper
                     elevation={0}
                     component="form"
                     className={classes.root}
@@ -38,15 +75,21 @@ const SearchField = () => {
                     <InputBase
                         className={classes.input}
                         placeholder="Search Cities..."
+                        value={city}
+                        onChange={handleSearchChange}
                     />
                     <IconButton
                         type="submit"
                         className={classes.iconButton}
                         aria-label="search"
-                    >
+                        onClick={handleSearch}
+                >
                         <SearchIcon style={{ color: 'white' }}/>
                     </IconButton>
-                </Paper>
+
+                </Paper>*/}
+                <input type="text" name="name" value={city} onChange={handleSearchChange}/>
+                <button>Search</button>
             </form>
         </div>
     );
